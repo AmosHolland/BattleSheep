5c3d4d3f5f4fba77ce53b56969cab0f8e3bd355e
AmosH <vo21765@bristol.ac.uk>
2023-03-16 15:33:09 +0000

    finished basic components

--- GameBoard.py
+++ GameBoard.py
@@ -1,4 +1,22 @@
 class GameBoard:
 
     def __init__(self, root_tile):
+
         coords = root_tile.get_coords(0, 0)
+        x_min = 0
+        y_min = 0
+
+        for i in coords:
+            x = i[1]
+            y = i[2]
+
+            if x < x_min:
+                x_min = x
+            if y < y_min:
+                y_min = y
+        
+        self.board = [[]]
+
+        for i in coords:
+            self.board[i[2] - y_min][i[1] - x_min] = i[0]
+        
