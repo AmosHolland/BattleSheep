d8587cd4e80e1ef3fb107c818c4e617c50300fe7
AmosH <vo21765@bristol.ac.uk>
2023-03-20 17:05:14 +0000

    Ahahah

--- GameDisplay/MainGame.py
+++ GameDisplay/MainGame.py
@@ -1,32 +1,56 @@
 import pygame
 import math
+import sys 
 
-def draw_hex(centre_x, centre_y, size, surface):
-    vertices = []
-    for i in range(6):
-        angle = (math.pi / 180) * (60 * i)
-        vertices.append((centre_x + size * math.cos(angle), centre_y + size * math.sin(angle)))
+sys.path.append("..")
 
-    pygame.draw.polygon(surface, (0, 154, 23), vertices)
+from GameSystem import GameBoard, GameHex, SheepStack
 
-    prev = vertices[0]
-    for i in range(1, 6):
-        pygame.draw.line(surface, (0,0,0), prev, vertices[i])
-        prev = vertices[i]
 
-    pygame.draw.line(surface, (0,0,0), vertices[5], vertices[0])
+class BattleSheepGame:
 
-def draw_hex_grid(width, height, size, surface):
-    centre_x = size
-    for x in range(width):
-        if x % 2 == 0:
-            centre_y = size 
-        else:
-            centre_y = 0.5 * math.sqrt(3) * size + size
-        for y in range(height):
-            draw_hex(centre_x, centre_y, size, surface)
-            centre_y += math.sqrt(3) * size
-        centre_x += 1.5 * size
+    def __init__(self, players, screen):
+        self.HEX_SIZE = 20
+        self.GRASS_COLOUR = (0, 154, 23)
+
+
+        self.screen = screen
+        self.players = players 
+        self.board = GameBoard.GameBoard()
+    
+    def draw_hex(self, centre_x, centre_y):
+        vertices = []
+        for i in range(6):
+            angle = (math.pi / 180) * (60 * i)
+            vertices.append((centre_x + self.HEX_SIZE * math.cos(angle), centre_y + self.HEX_SIZE * math.sin(angle)))
+
+        pygame.draw.polygon(self.screen, self.GRASS_COLOUR, vertices)
+
+        prev = vertices[0]
+        for i in range(1, 6):
+            pygame.draw.line(self.screen, (0,0,0), prev, vertices[i])
+            prev = vertices[i]
+        
+        pygame.draw.line(self.screen, (0,0,0), vertices[0], vertices[5])
+    
+    def draw_board(self):
+
+        centre_x = self.HEX_SIZE
+        hex_board = self.board.hex_board
+        print(hex_board)
+        for x in range(len(hex_board)):
+            if x % 2 == 0:
+                centre_y = self.HEX_SIZE
+            else:
+                centre_y = 0.5 * math.sqrt(3) * self.HEX_SIZE + self.HEX_SIZE
+            for y in range(len(hex_board[x])):
+                if hex_board[x][y] != None:
+                    self.draw_hex(centre_x, centre_y)
+                centre_y += math.sqrt(3) * self.HEX_SIZE
+            centre_x += 1.5 * self.HEX_SIZE
+
+    def make_dummy_board(self, grid):
+        self.board.hex_board = grid
         
 
 # Define the background colour
@@ -50,8 +74,11 @@ screen.fill(background_colour)
   
 # Variable to keep our game loop running
 running = True
-grid = [[0, 0, 0, 0, 0], [0, 0, 0], [0, 0, 0]]
-draw_hex_grid(20, 30, 15, screen)
+grid = [[0, 0, 0, 0, 0], [0, None, 0], [0, 0, 0]]
+
+game = BattleSheepGame(2, screen)
+game.make_dummy_board(grid)
+game.draw_board()
 
 pygame.display.flip()
 # game loop
